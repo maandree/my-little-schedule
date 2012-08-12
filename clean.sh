aeaa78b145d91132c3247df979d40463968ffd05
Mattias Andrée <maandree@operamail.com>
2012-08-12 18:38:44 +0200

    adding gitignore + adding clean script

---
+++ clean.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This script is put in Public Domain
+# Year: 2012
+# Author: Mattias Andrée (maandree@kth.se)
+
+
+rm -r bin  2>  /dev/null
+
+rm $(find . | grep '~$')        2>  /dev/null
+rm $(find . | grep '\.class$')  2>  /dev/null
+rm $(find . | egrep '/(.|)#')   2>  /dev/null
+
+for file in $(find src | grep '\.java$'); do
+    expand $file > $file"~"
+    mv $file"~" $file
+    git add $file
+done
+
